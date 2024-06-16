fa7eafed95f088accc962dba6e1d7f370ad8f37e
Mariana <mariananavarro3022@gmail.com>
2024-06-16 00:28:07 -0600

    sirve :) pero no como deberia :(

--- src/contexts/buzon.js
+++ src/contexts/buzon.js
@@ -6,7 +6,25 @@
 
 
 
- export const addMessageToFirestore = async (nombre, nombre2, apellido1, apellido2, date, hour, content, state) => {
+//  export const addMessageToFirestore = async (nombre, nombre2, apellido1, apellido2, date, hour, content, state) => {
+//     try {
+//         const formatomsg = {
+//             emisor: `${nombre} ${nombre2} ${apellido1} ${apellido2}`,
+//             fecha: date,
+//             hora: hour,
+//             contenido: content,
+//             estado: state
+//         };
+
+//         await addDoc(collection(db, 'message'), formatomsg);
+//         console.log("Message added successfully");
+    
+//     } catch (error) {
+//         console.error("Error adding document: ", error);
+//     }
+// };
+
+export const addMessageToFirestore = async (nombre, nombre2, apellido1, apellido2, date, hour, content, state) => {
     try {
         const formatomsg = {
             emisor: `${nombre} ${nombre2} ${apellido1} ${apellido2}`,
@@ -16,14 +34,100 @@
             estado: state
         };
 
-        await addDoc(collection(db, 'message'), formatomsg);
+        // Agregar el mensaje a la colección 'message'
+        const messageDocRef = await addDoc(collection(db, 'message'), formatomsg);
         console.log("Message added successfully");
-    
+
+        // Obtener correos de los estudiantes
+        const emailEstudiantes = await getEstudiantes();
+
+        // Para cada correo electrónico de estudiante proporcionado
+        for (const estudiante of emailEstudiantes) {
+            const emailEstudiante = estudiante.email;
+            // Obtener una referencia a la colección 'messageStudent' del estudiante
+            const messageStudentRef = doc(collection(db, 'messageStudent'), emailEstudiante);
+
+            // Obtener el documento de 'messageStudent'
+            const messageStudentDoc = await getDoc(messageStudentRef);
+
+            // Obtener la lista de mensajes del estudiante o inicializarla si es la primera vez
+            let listaMensajes = [];
+            if (messageStudentDoc.exists()) {
+                listaMensajes = messageStudentDoc.data().listamensajes || [];
+            }
+
+            // Agregar el ID del nuevo mensaje a la lista de mensajes del estudiante
+            listaMensajes.push(messageDocRef.id);
+
+            // Actualizar el documento 'messageStudent' con la nueva lista de mensajes
+            await setDoc(messageStudentRef, { correo: emailEstudiante, listamensajes: listaMensajes });
+            console.log("MessageStudent updated successfully with new message");
+        }
     } catch (error) {
         console.error("Error adding document: ", error);
     }
 };
 
+export const getEstudiantes = async () => {
+    try {
+        // Obtener una referencia a la colección de estudiantes en Firestore
+        const estudiantesCollection = collection(db, 'Estudiantes');
+
+        // Consultar los documentos de la colección
+        const querySnapshot = await getDocs(query(estudiantesCollection));
+
+        // Crear un array para almacenar los estudiantes
+        const estudiantes = [];
+
+        // Iterar sobre los documentos y agregar los datos de cada estudiante al array
+        querySnapshot.forEach((doc) => {
+            estudiantes.push({
+                email: doc.data().email
+            });
+        });
+
+        console.log("Estudiantes obtenidos y ordenados exitosamente:", estudiantes);
+        return estudiantes;
+    } catch (error) {
+        console.error("Error al obtener y ordenar los estudiantes:", error);
+        throw error; // Propagar el error para que sea manejado por el código que llama a esta función
+    }
+};
+
+export const getMessagesByEmail = async (emailEstudiante) => {
+    try {
+        // Obtener la referencia al documento en 'messageStudent' correspondiente al correo del estudiante
+        const messageStudentRef = doc(db, 'messageStudent', emailEstudiante);
+        
+        // Obtener el documento
+        const messageStudentDoc = await getDoc(messageStudentRef);
+
+        // Verificar si el documento existe
+        if (!messageStudentDoc.exists()) {
+            console.log("No messages found for this email");
+            return [];
+        }
+
+        // Obtener la lista de IDs de mensajes
+        const { listamensajes } = messageStudentDoc.data();
+
+        // Obtener los detalles de cada mensaje en la colección 'message'
+        const messages = [];
+        for (const messageId of listamensajes) {
+            const messageDoc = await getDoc(doc(db, 'message', messageId));
+            if (messageDoc.exists()) {
+                messages.push({ id: messageDoc.id, ...messageDoc.data() });
+            } else {
+                console.log(`Message with ID ${messageId} does not exist`);
+            }
+        }
+
+        return messages;
+    } catch (error) {
+        console.error("Error getting messages: ", error);
+        throw error; // Propagar el error para manejarlo en el llamado de la función
+    }
+};
 
 export const getMessagesFromFirestore = async () => {
     try {
@@ -34,7 +138,6 @@ export const getMessagesFromFirestore = async () => {
             ref: doc.ref,  // Agregar la referencia al documento
             ...doc.data()
         }));
-
         return messagesList;
     } catch (error) {
         console.error("Error getting documents: ", error);
@@ -52,3 +155,5 @@ export const updateMessageInFirestore = async (messageId, newMessage) => {
         console.error("Error updating document: ", error);
     }
 };
+
+
--- src/pages/BuzonEntrada/BuzonEntrada.js
+++ src/pages/BuzonEntrada/BuzonEntrada.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import NavBar from "../../components/navBar/navBar";
 import Message from './Message';
-import { addMessageToFirestore, getMessagesFromFirestore  } from "../../contexts/buzon";
+import { addMessageToFirestore, getMessagesFromFirestore, getMessagesByEmail } from "../../contexts/buzon";
 
 import './BuzonEntrada.css'; 
 
@@ -13,7 +13,7 @@ const BuzonEntrada = () => {
     useEffect(() => {
         const fetchMessages = async () => {
             try {
-                const messages = await getMessagesFromFirestore();
+                const messages = await await getMessagesByEmail('gnunez@estudiantec.cr');
                 setMessages(messages);
             } catch (error) {
                 console.error('Error getting messages:', error);
@@ -51,6 +51,62 @@ const BuzonEntrada = () => {
     );
 };
 
+// 0
+// : 
+// {email: 'mnavarro@estudiantec.cr'}
+// 1
+// : 
+// {email: 'gnunez@estudiantec.cr'}
+// 2
+// : 
+// {email: 'amolina@estudiantec.cr'}
+// 3
+// : 
+// {email: 'avega@estudiantec.cr'}
+// 4
+// : 
+// {email: 'gfiatt@estudiantec.cr'}
+// 5
+// : 
+// {email: 'jsanchez@estudiantec.cr'}
+// 6
+// : 
+// {email: 'jvillalta@estudiantec.cr'}
+// 7
+// : 
+// {email: 'aaraya@estudiantec.cr'}
+// const BuzonEntrada = () => {
+//     const [messages, setMessages] = useState([
+//         { id: 1, sender: 'Place Holder Name', text: 'Example open text Example open text Example open text Example open textExample open textv Example open text Example open text Example open text Example open text Example open text Example open text Example open text Example open text Example open text Example open text Example open text', date: 'Date - time', isRead: false },
+//         { id: 2, sender: 'Place Holder Name', text: 'Place holder text', date: 'Date - time', isRead: false },
+//         // Add more messages as needed
+//     ]);
+
+//     const handleAddMessage = async () => {
+//         try {
+//             const fetchedMessages = await getMessagesByEmail('mnavarro@estudiantec.cr');
+//             console.log(fetchedMessages);
+//         } catch (error) {
+//             console.error('Error fetching messages:', error);
+//         }
+//     };
+//     return (
+//         <>
+//             <Helmet>
+//                 <title>BuzonEntrada</title>
+//             </Helmet>
+//             <div>
+//                 <NavBar titulo="BuzonEntrada" />
+//                 <button onClick={handleAddMessage}>Add Message</button>
+//                 <div className="buzon-container">
+//                     {messages.map((message) => (
+//                         <Message key={message.id} message={message} />
+//                     ))}
+//                 </div>
+//             </div>
+//         </>
+//     );
+// };
 
 // const BuzonEntrada = () => {
 //     const [messages, setMessages] = useState([
